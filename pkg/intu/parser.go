b25ad38f6478c599b6c4bb9fa6f6bc5f2bd2b73c
Matt Michie <mmichie@gmail.com>
2024-06-27 18:51:29 -0700

    refactor(parser): remove debug logging statements
    - Remove log.Printf statements from ParseTaggedContent function
    - Simplify code by eliminating unnecessary debug outputs

--- pkg/intu/parser.go
+++ pkg/intu/parser.go
@@ -2,27 +2,21 @@ package intu
 
 import (
 	"fmt"
-	"log"
 	"regexp"
 	"strings"
 )
 
 // ParseTaggedContent extracts content between specified XML-like tags
 func ParseTaggedContent(input, tag string) (string, error) {
-	log.Printf("Debug: Parsing content for tag: %s", tag)
-	log.Printf("Debug: Input: %s", input)
-
 	// Look for the tags within the input, ignoring any text before or after
 	pattern := fmt.Sprintf(`(?s)<%s>\n?(.*?)\n?</%s>`, tag, tag)
 	re := regexp.MustCompile(pattern)
 	matches := re.FindStringSubmatch(input)
 	if len(matches) == 2 {
-		log.Printf("Debug: Match found")
 		return strings.TrimSpace(matches[1]), nil
 	}
 
 	// If we haven't found anything, return an error
-	log.Printf("Debug: No match found")
 	return "", fmt.Errorf("no content found between <%s> tags", tag)
 }
 
